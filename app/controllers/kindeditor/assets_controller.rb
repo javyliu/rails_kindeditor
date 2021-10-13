05505e97c4bcf1a71f905a6991c4d3278d3aa023
qmliu <qmliu@pearlinpalm.com>
2021-10-13 16:43:07 +0800

    upgrade to ruby-3.0.0@rails6.1

--- app/controllers/kindeditor/assets_controller.rb
+++ app/controllers/kindeditor/assets_controller.rb
@@ -1,7 +1,7 @@
 #coding: utf-8
 require "find"
 class Kindeditor::AssetsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   def create
     @imgFile, @dir, @fixed_folder,@digest_filename = params[:imgFile], params[:dir], params[:fixed_folder],params[:digest_filename]
 
