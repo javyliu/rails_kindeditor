2d9a04e45ae74c4cdd8e768c2bb3b3900309ce21
qmliu <qmliu@pearlinpalm.com>
2024-08-28 16:04:29 +0800

    rails 5 不再使用 render :text => xx, 更改为 render :plain => “

--- app/controllers/kindeditor/assets_controller.rb
+++ app/controllers/kindeditor/assets_controller.rb
@@ -15,7 +15,7 @@ class Kindeditor::AssetsController < ApplicationController
           logger.warn '========= Warning: the owner_id is 0, "delete uploaded files automatically" will not work. =========' if defined?(logger) && @asset.owner_id == 0
           @asset.asset_type = @dir
           if @asset.save
-            render :text => ({:error => 0, :url => @asset.asset.url}.to_json)
+            render :plain => ({:error => 0, :url => @asset.asset.url}.to_json)
           else
             show_error(@asset.errors.full_messages)
           end
@@ -29,7 +29,7 @@ class Kindeditor::AssetsController < ApplicationController
           uploader.fixed_folder = @fixed_folder
           uploader.digest_filename = @digest_filename
           uploader.store!(@imgFile)
-          render :text => ({:error => 0, :url => uploader.url}.to_json)
+          render :plain => ({:error => 0, :url => uploader.url}.to_json)
         rescue CarrierWave::UploadError => e
           show_error(e.message)
         rescue Exception => e
@@ -51,7 +51,7 @@ class Kindeditor::AssetsController < ApplicationController
     @img_ext = Kindeditor::AssetUploader::EXT_NAMES[:image]
     @dir = params[:dir].strip || ""
     unless Kindeditor::AssetUploader::EXT_NAMES.keys.map(&:to_s).push("").include?(@dir)
-      render :text => "Invalid Directory name."
+      render :plain => "Invalid Directory name."
       return
     end
 
@@ -80,15 +80,15 @@ class Kindeditor::AssetsController < ApplicationController
     end
     @order = %w(name size type).include?(params[:order].downcase) ? params[:order].downcase : "name"
     if !@current_path.match(/\.\./).nil?
-      render :text => "Access is not allowed."
+      render :plain => "Access is not allowed."
       return
     end
     if @current_path.match(/\/$/).nil?
-      render :text => "Parameter is not valid."
+      render :plain => "Parameter is not valid."
       return
     end
     if !File.exist?(@current_path) || !File.directory?(@current_path)
-      render :text => "Directory does not exist."
+      render :plain => "Directory does not exist."
       return
     end
     @file_list = []
@@ -125,12 +125,12 @@ class Kindeditor::AssetsController < ApplicationController
     @result[:current_url] = @current_url
     @result[:total_count] = @file_list.count
     @result[:file_list] = @file_list
-    render :text => @result.to_json
+    render :plain => @result.to_json
   end
 
   private
   def show_error(msg)
-    render :text => ({:error => 1, :message => msg}.to_json)
+    render :plain => ({:error => 1, :message => msg}.to_json)
   end
 
 end
